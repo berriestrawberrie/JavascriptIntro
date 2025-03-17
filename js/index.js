2a0971dcf128ace51c05f87a1667122e45779926
Brooke Brown <brooke.henriksson@outlook.com>
2025-03-17 10:21:33 +0100

    Completed exercise 1 - 3 in javascript

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 //EXERCISE 1
-console.log("Hello World! ❤")
+console.log("Hello World! ❤");
+
+//EXERCISE 2
+let age = 33;
+let name = 'Brooke';
+let color = 'pink';
+console.log(`My name is ${name} I am ${age} years old and my favorite color is ${color}`);
+
+//EXERCISE 3
+let x = 10;
+let y = x * 2;
+
+
+console.log(x+y);
+console.log(y-x);
+console.log(x/y);
+console.log(y*x);
+
+
+
